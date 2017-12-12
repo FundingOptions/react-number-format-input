322a0a6def6fbb6c5993698a509e95ecdda8f017
Anthony King <anthony.king@fundingoptions.com>
2017-12-12 15:48:54 +0000

    fix demo

--- demo/Example.js
+++ demo/Example.js
@@ -1,5 +1,6 @@
 import NumberFormatInput from '../src';
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 const simpleFormat = new Intl.NumberFormat('en-US');
 
